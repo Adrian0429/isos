912c5f9f92590b84454b313c4823b07459bbcfbc
Adrian Karuna Soetikno <royankaruna@gmail.com>
2025-10-01 09:13:06 +0900

    added logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { getSheetData } from "./lib/googleSheets";
 
 export default async function Page() {
   const rows = await getSheetData();
-
+  console.log("data", rows)
   const queues = rows.map((row) => row[0]); // take first column as queue numbers
   const initialQueue = queues[1] || "Empty Queue";
 
